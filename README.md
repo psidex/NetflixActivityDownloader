81aac8cbd87e65ff27a1f4fbe05daf83a6227ec9
thatguywiththatname <simjenner3@gmail.com>
2019-05-15 16:44:40 +0100

    update from old GH username

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Use of this software may constitute a breach in the [Netflix Terms of Use](https
  - Highlight and copy **ALL** the code below 
 
  ```javascript
-javascript:var s=document.createElement("script");s.src="https://thatguywiththatname.github.io/NetflixStats/minified/NetflixStats.js",s.type="application/javascript",document.getElementsByTagName("head")[0].appendChild(s);void 0
+javascript:var s=document.createElement("script");s.src="https://psidex.github.io/NetflixStats/minified/NetflixStats.js",s.type="application/javascript",document.getElementsByTagName("head")[0].appendChild(s);void 0
 ```
 
  - Create a new bookmark in your browser, and where the URL goes, paste the code in
--- source/PreLoad.js
+++ source/PreLoad.js
@@ -21,9 +21,9 @@ document.getElementsByTagName("head")[0].appendChild(script);
 // Insert custom CSS, remove content in page and add loading symbol
 // The CSS is handled by the build script
 document.head.innerHTML += `{injectCSS}`;
-document.getElementsByClassName("bd")[0].innerHTML = '<div id="NetflixStats"><h1>Gathering Stats</h1><br/><img height="100" width="100" src="https://thatguywiththatname.github.io/NetflixStats/source/images/loader.gif"></div>';
+document.getElementsByClassName("bd")[0].innerHTML = '<div id="NetflixStats"><h1>Gathering Stats</h1><br/><img height="100" width="100" src="https://psidex.github.io/NetflixStats/source/images/loader.gif"></div>';
 document.getElementsByClassName("site-footer-wrapper")[0].innerHTML = "";
 
 
 
-{nextScript}
+{ nextScript }
