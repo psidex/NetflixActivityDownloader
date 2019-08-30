d498fe5668a90330dd6f8f88cbdc973c9e16c16e
thatguywiththatname <simjenner3@gmail.com>
2019-08-31 00:05:18 +0100

    Updated NF Stats to use async/await syntax, Changed build.py to be a constant watching process.

--- build.py
+++ build.py
@@ -3,38 +3,69 @@ The jsmin library isn't perfect but it's good enough.
 """
 
 import os
-import shutil
 import pathlib
 import logging
+import time
+import sys
+
 from jsmin import jsmin
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+DIR_MINIFIED = pathlib.Path("minified")
+DIR_SOURCE = pathlib.Path("src")
 
 
-def main() -> None:
-    dir_minified = pathlib.Path("minified")
-    dir_source = pathlib.Path("src")
+class MinifyNetflixStatsEventHandler(FileSystemEventHandler):
+    def dispatch(self, event):
+        """Minify NetflixStats.js inside /src to /minified"""
+        if event.is_directory and not event.src_path.endswith("NetflixStats.js"):
+            return  # Don't do anything if NetflixStats didn't change
 
-    if os.path.exists(dir_minified):
-        shutil.rmtree(dir_minified)
-        logging.info("Removed /minified")
+        logging.info("Detected change in NetflixStats.js")
 
-    os.makedirs(dir_minified)
-    logging.info("Created /minified")
+        if not os.path.exists(DIR_MINIFIED):
+            os.makedirs(DIR_MINIFIED)
+            logging.info("Created /minified")
 
-    logging.info("Reading NetflixStats.js")
-    with open(dir_source.joinpath("NetflixStats.js"), "r") as in_file:
-        normal_js = in_file.read()
+        logging.info("Reading NetflixStats.js")
+        with open(DIR_SOURCE.joinpath("NetflixStats.js"), "r") as in_file:
+            normal_js = in_file.read()
 
-    logging.info("Minifying")
-    minified = jsmin(normal_js, quote_chars="'\"`")  # quote_chars to support ES6
+        logging.info("Minifying")
+        minified = jsmin(normal_js, quote_chars="'\"`")  # quote_chars to support ES6
 
-    logging.info("Writing minified file to /minified")
-    with open(dir_minified.joinpath("NetflixStats.min.js"), "w") as out_file:
-        out_file.write(minified)
+        logging.info("Writing minified file to /minified")
+        with open(DIR_MINIFIED.joinpath("NetflixStats.min.js"), "w") as out_file:
+            out_file.write(minified)
 
-    logging.info("Finished")
+        logging.info("Finished")
+
+
+def startFileWatcher():
+    """Watch /src for changes and minify NetflixStats.js on a change"""
+    path = str(DIR_SOURCE)
+    event_handler = MinifyNetflixStatsEventHandler()
+
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=False)
+    observer.start()
+
+    logging.info("Watching /src for changes")
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    logging.info("Stopping")
+    observer.join()
 
 
 if __name__ == "__main__":
-    logging_root = logging.getLogger()
-    logging_root.setLevel(logging.DEBUG)
-    main()
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    startFileWatcher()
--- src/NetflixStats.js
+++ src/NetflixStats.js
@@ -1,8 +1,154 @@
-function showInfo() {
-    /* Inserts the HTML that the user will see that contains all the info 
-     * Performs calculations on the flixStats data and displays it in Chart.js charts
+function startLoadingUI() {
+    /**
+     * - Insert custom CSS
+     * - Insert loading symbol
+     * - Remove the footer (as it gets in the way)
      */
+    document.head.innerHTML += `
+    <style>
+    h1 {
+        color: #000;
+    }
+    #NetflixStats {
+        padding-top: 10px;
+        text-align: center;
+        position: absolute;
+        width: 95%;
+        opacity: 1;
+        z-index: 1;
+        background: RGB(243, 243, 243);
+    }
+    .chart-container {
+        width: 600px;
+        height: 300px;
+        text-align: center;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    </style>`;
 
+    document.getElementsByClassName("bd")[0].innerHTML = `
+    <div id="NetflixStats">
+        <h1>Gathering Stats</h1>
+        <br />
+        <!-- Uses Netflix's built-in loading symbol -->
+        <div><div class="basic-spinner"></div></div>
+    </div>`;
+
+    document.getElementsByClassName("site-footer-wrapper")[0].innerHTML = "";
+}
+
+function setupNetflixVariables() {
+    /**
+     * Sets 2 global window variables:
+     * - shaktiHistoryURL which is the URL to get Netflix history
+     * - flixStats which will contain all gathered info
+     */
+    let flixInfo = window.netflix.reactContext.models.serverDefs.data;
+    let userInfo = window.netflix.reactContext.models.userInfo.data;
+    window.shaktiHistoryURL = flixInfo.API_ROOT + "/shakti/" + flixInfo.BUILD_IDENTIFIER + "/viewingactivity?authURL=" + userInfo.authURL + "&pg=";
+    window.flixStats = {
+        viewedItems: {},
+        userInfo: userInfo
+    };
+}
+
+function loadChartJS() {
+    /**
+     * Load the Chart.js library from jsdelivr - uses a Promise for the onload resolution
+     */
+    return new Promise((resolve) => {
+        let chartJSScript = document.createElement("script");
+        chartJSScript.type = "application/javascript";
+        chartJSScript.src = "https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.js";
+        chartJSScript.onload = () => {
+            console.log("Chart.js loaded")
+            resolve();
+        };
+        document.head.appendChild(chartJSScript);
+    });
+}
+
+async function gatherWatchInfo() {
+    /**
+     * Load data about all watched series / films into the global flixStats object
+     */
+    let currentPage = 0;
+    console.log("Using API URL: " + shaktiHistoryURL);
+
+    while (true) {
+        let response = await fetch(shaktiHistoryURL + currentPage);
+
+        if (response.ok) {
+            let data = await response.json();
+
+            if (data.viewedItems[0] === undefined) {
+                console.log("Finished gathering data");
+                return;
+
+            } else {
+                console.log("Got data for page " + currentPage);
+
+                for (let i = 0; i < data.viewedItems.length; i++) {
+                    let itemData = data.viewedItems[i];
+
+                    // Extract title, ID, and type of series / film
+
+                    // For series `movieID` will be the ID of the individual episode
+                    let itemMovieID = itemData.movieID;
+
+                    if (itemData.seriesTitle) {
+                        var itemUniqueID = itemData.series;
+                        var itemTitle = itemData.seriesTitle
+                        var itemType = "series";
+                        // Get some details about the episode watched if it is part of a series
+                        var episodeData = {
+                            "title": itemData.title,
+                            "dateWatched": itemData.dateStr,
+                            "duration": itemData.duration
+                        };
+
+                    } else {
+                        var itemUniqueID = itemMovieID;
+                        var itemTitle = itemData.videoTitle
+                        var itemType = "film";
+                    }
+
+                    if (!flixStats.viewedItems[itemUniqueID]) {
+                        flixStats.viewedItems[itemUniqueID] = {}
+                        flixStats.viewedItems[itemUniqueID].title = itemTitle;
+                        flixStats.viewedItems[itemUniqueID].type = itemType;
+
+                        if (itemType == "series") {
+                            flixStats.viewedItems[itemUniqueID].watchedEpisodes = {
+                                itemMovieID: episodeData
+                            };
+
+                        } else {
+                            flixStats.viewedItems[itemUniqueID].dateWatched = itemData.dateStr;
+                            flixStats.viewedItems[itemUniqueID].duration = itemData.duration;
+                        }
+
+                    } else {
+                        // Update data - Netflix doesen't track repeated watches of
+                        // films, so only series will be changed here
+                        flixStats.viewedItems[itemUniqueID].watchedEpisodes[itemMovieID] = episodeData;
+                    }
+                }
+            }
+
+        } else {
+            return alert("NetflixStats fetch Error: " + response.status);
+        }
+
+        currentPage++;
+    }
+}
+
+function showInfo() {
+    /**
+     * Shows info
+     */
     // {seriesName: secondsWatched}
     var nameToWatched = {};
     // {dateString: secondsWatched}
@@ -54,8 +200,7 @@ function showInfo() {
     }
 
     // Insert HTML for the stats
-    var NetflixStatsObject = document.getElementById("NetflixStats");
-    NetflixStatsObject.innerHTML = `
+    document.getElementById("NetflixStats").innerHTML = `
     <h1>Netflix Stats for ${flixStats.userInfo.name}</h1>
     <p>
         <sup><a href="https://psidex.github.io/NetflixStats/#notes--faq">FAQ</a></sup>
@@ -79,7 +224,7 @@ function showInfo() {
     // Create and draw all the charts
 
     let top5WatchedShowsChartCTX = document.getElementById("top5WatchedShowsChart").getContext("2d");
-    let top5WatchedShowsChart = new Chart(top5WatchedShowsChartCTX, {
+    new Chart(top5WatchedShowsChartCTX, {
         type: "bar",
         data: {
             labels: top5WatchedShows,
@@ -105,7 +250,7 @@ function showInfo() {
     });
 
     let top5WatchedDatesChartCTX = document.getElementById("top5WatchedDatesChart").getContext("2d");
-    let top5WatchedDatesChart = new Chart(top5WatchedDatesChartCTX, {
+    new Chart(top5WatchedDatesChartCTX, {
         type: "bar",
         data: {
             labels: top5WatchedDates,
@@ -131,164 +276,12 @@ function showInfo() {
     });
 }
 
-function gatherWatchInfo(callback, currentPage = 0) {
-    /* Iterates through all watch history pages and pulls the needed info
-     * Populates the global flixStats object
-     */
-
-    console.log("Getting page " + currentPage);
-
-    fetch(shaktiHistoryURL + currentPage)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            if (data.viewedItems[0] === undefined) {
-                console.log("No viewed items in page, finished gathering pages");
-                callback();
-            } else {
-                // For each item in the data
-                for (let i = 0; i < data.viewedItems.length; i++) {
-                    let itemData = data.viewedItems[i];
-
-                    // Extract title, ID, and type of series / film
-
-                    // For series `movieID` will be the ID of the individual episode
-                    let itemMovieID = itemData.movieID;
-
-                    if (itemData.seriesTitle) {
-                        var itemUniqueID = itemData.series;
-                        var itemTitle = itemData.seriesTitle
-                        var itemType = "series";
-                        // Get some details about the episode watched if it is part of a series
-                        var episodeData = {
-                            "title": itemData.title,
-                            "dateWatched": itemData.dateStr,
-                            "duration": itemData.duration
-                        };
-                    } else {
-                        var itemUniqueID = itemMovieID;
-                        var itemTitle = itemData.videoTitle
-                        var itemType = "film";
-                    }
-
-                    if (!flixStats.viewedItems[itemUniqueID]) {
-                        flixStats.viewedItems[itemUniqueID] = {}
-                        flixStats.viewedItems[itemUniqueID].title = itemTitle;
-                        flixStats.viewedItems[itemUniqueID].type = itemType;
-
-                        if (itemType == "series") {
-                            flixStats.viewedItems[itemUniqueID].watchedEpisodes = {
-                                itemMovieID: episodeData
-                            };
-                        } else {
-                            flixStats.viewedItems[itemUniqueID].dateWatched = itemData.dateStr;
-                            flixStats.viewedItems[itemUniqueID].duration = itemData.duration;
-                        }
-
-                    } else {
-                        // Update data - Netflix doesen't track repeated watches of
-                        // films, so only series will be changed here
-                        flixStats.viewedItems[itemUniqueID].watchedEpisodes[itemMovieID] = episodeData;
-                    }
-                }
-
-                gatherWatchInfo(callback, currentPage + 1);
-            }
-        });
-}
-
-function changeUI() {
-    /* Does several things that happen before loading the data:
-     * - Insert custom CSS
-     * - Insert loading symbol
-     * - Remove the footer (as it gets in the way)
-     */
-    document.head.innerHTML += `
-    <style>
-    h1 {
-        color: #000;
-    }
-
-    #NetflixStats {
-        padding-top: 10px;
-        text-align: center;
-        position: absolute;
-        width: 95%;
-        opacity: 1;
-        z-index: 1;
-        background: RGB(243, 243, 243);
-    }
-
-    .chart-container {
-        width: 600px;
-        height: 300px;
-        text-align: center;
-        margin-left: auto;
-        margin-right: auto;
-    }
-    </style>`;
-
-    document.getElementsByClassName("bd")[0].innerHTML = `
-    <div id="NetflixStats">
-        <h1>Gathering Stats</h1>
-        <br />
-        <!-- Uses Netflix's built-in loading symbol -->
-        <div><div class="basic-spinner"></div></div>
-    </div>`;
-
-    document.getElementsByClassName("site-footer-wrapper")[0].innerHTML = "";
-}
-
-function loadExternal() {
-    /* Load the Chart.js library before doing anything, so that it can be used later
-     */
-    return new Promise((resolve, reject) => {
-        let chartJSScript = document.createElement("script");
-        chartJSScript.type = "application/javascript";
-        chartJSScript.src = "https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.js";
-        chartJSScript.onload = () => {
-            console.log("Chart.js loaded")
-            resolve();
-        };
-        document.head.appendChild(chartJSScript);
-    });
-}
-
-function setupNetflixVariables() {
-    /* Sets up 2 global variables to be used later, shaktiHistoryURL and flixStats
-     */
-
-    // Shakti API stuff
-    let flixInfo = window.netflix.reactContext.models.serverDefs.data;
-    let userInfo = window.netflix.reactContext.models.userInfo.data;
-
-    // A global variable containing the Shakti API URL for watch history
-    window.shaktiHistoryURL = flixInfo.API_ROOT + "/shakti/" + flixInfo.BUILD_IDENTIFIER + "/viewingactivity?authURL=" + userInfo.authURL + "&pg=";
-
-    // A global variable that will contain all gathered info
-    window.flixStats = {
-        viewedItems: {},
-        userInfo: userInfo
-    };
-
-    console.log("Using API URL: " + shaktiHistoryURL);
-}
-
-function main() {
-    loadExternal()
-        .then(() => {
-            changeUI();
-        })
-        .then(() => {
-            setupNetflixVariables();
-        })
-        .then(() => {
-            gatherWatchInfo(() => {
-                console.log("Finished gathering data");
-                showInfo();
-            });
-        });
+async function main() {
+    startLoadingUI();
+    setupNetflixVariables();
+    await loadChartJS()
+    await gatherWatchInfo(); //alriight
+    showInfo();
 }
 
 main();
