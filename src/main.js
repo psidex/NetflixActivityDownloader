d14a440e164a9242e0dc28cae46ea8603f96daf3
thatguywiththatname <simjenner3@gmail.com>
2020-04-03 21:41:33 +0100

    Update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { getVarsFromReact } from './variables.js';
 import { downloadActivity } from './api.js';
 
 async function main() {
+    console.log('Loaded NetflixStats');
     const { shaktiActivityUrl, userInfo } = getVarsFromReact();
     const activity = await downloadActivity(shaktiActivityUrl);
     console.log(userInfo);
